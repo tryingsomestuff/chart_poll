2c2f4a92b632bd9241da39715a185f2c21fffcd8
Vivien Clauzon <xr_a_y@yahoo.fr>
2024-11-16 17:05:07 +0100

    limit to 3 points per connexion

--- server.js
+++ server.js
@@ -7,20 +7,28 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 let points = [];
+const clientPointLimits = {};
 
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
-    console.log('New user conencted');
+    console.log('New user connected');
+    clientPointLimits[socket.id] = 0;
     socket.emit('updatePoints', points);
 
     socket.on('newPoint', (point) => {
-        points.push(point);
-        io.emit('updatePoints', points);
+        if (clientPointLimits[socket.id] < 3) {
+            clientPointLimits[socket.id] += 1;
+            points.push(point);
+            io.emit('updatePoints', points);
+        } else {
+            socket.emit('error', 'You can only add up to 3 points.');
+        }
     });
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
+        delete clientPointLimits[socket.id];
     });
 });
 
